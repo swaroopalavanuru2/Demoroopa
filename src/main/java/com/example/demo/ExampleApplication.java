cb66b2bfea7bdd595876db71412ef61abda9546b
Srikanth JADALA <swaroopal.in@mouritech.com>
2025-08-01 07:21:51 +0530

    commit

---
+++ src/main/java/com/example/demo/ExampleApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class ExampleApplication {
+	@GetMapping("/playing-java")
+	public String getData() {
+		return "Springboot Application Running with  docker on ec2 services in aws";
+		
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+}
